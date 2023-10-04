ecd669ba1513d4c93fb09b0c118af552f0295aa9
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-10-04 19:36:56 +0900

    Drill08제출(2020112036)정현도

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,44 @@
 from pico2d import *
+import random
 
 # Game object class here
+
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400, 30)
+    def update(self): pass
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        if self.x % 2 == 1:
+            self.image = load_image('ball41x41.png')
+        else:
+            self.image = load_image('ball21x21.png')
+
+    def update(self):
+        if(self.y > 70):
+            self.y -= random.randint(1, 20)
+        else:
+            self.y = 60
+        pass
+    def draw(self):
+        self.image.draw(self.x, self.y)
 
 def handle_events():
     global running
@@ -11,11 +49,44 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+def reset_world():
+    global running
+    global grass
+    global team
+    global world
+    global ball
+
+    running = True
+    world = []
+    grass = Grass()
+    world.append(grass)
+    team = [Boy() for i in range(10)]
+    world += team
+    ball = [Ball() for i in range(20)]
+    world += ball
+
+def update_world():
+    for o in world:
+        o.update()
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_world()  # game logic
+    render_world()  # draw game world
+    delay(0.05)
 
 # finalization code
 
